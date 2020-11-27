dd1f08317d4bbca60d17bc47677af821aff7cedf
brennop <brennopcordeiro@gmail.com>
2020-11-26 22:41:04 -0300

    ⭐ Updates favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,9 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: 'data:;base64,iVBORw0KGgo=' },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
