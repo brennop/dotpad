f3dc2954e89f31bc0631e66bb54ea24a00b5c2c2
brennop <brennopcordeiro@gmail.com>
2020-11-28 11:17:55 -0300

    🍤 Add description meta tag

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,11 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Collaborative rich text editing',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: 'data:;base64,iVBORw0KGgo=' },
