07de1782db6724781f5f646ad2e895b757a8e032
brennop <brennopcordeiro@gmail.com>
2022-01-27 09:52:24 -0300

    add home page

--- main.js
+++ main.js
@@ -8,65 +8,99 @@ import * as Y from "yjs";
 import { WebsocketProvider } from "y-websocket";
 import { IndexeddbPersistence } from "y-indexeddb";
 
-const INIT_CONN_TIMEOUT = 5000;
-const server = 'wss://dotpad.fly.dev';
+const INIT_CONN_TIMEOUT = 3000;
+const server = "wss://dotpad.fly.dev";
 
+const app = document.querySelector("#app");
 const name = location.pathname.slice(1);
 
-const doc = new Y.Doc();
-const websocket = new WebsocketProvider("ws://localhost:1234", name, doc);
-const provider = new IndexeddbPersistence(name, doc);
+if (name === "") {
+  const visited = localStorage.getItem("visited");
+  const visitedList = visited ? JSON.parse(visited) : [];
 
-async function notifyConnection() {
-  return new Promise((resolve, reject) => {
-    setTimeout(reject, INIT_CONN_TIMEOUT);
+  app.innerHTML = `
+    <div class="home">
+      <h1>dotpad</h1>
+      <form>
+        <input></input>
+        <button>go</button>
+      </form>
+      <h3>recent files</h3>
+      <ul class="visited-list">
+        ${visitedList
+      .map(
+        (link) => `
+          <li>
+            <a href="/${link}">
+              <p>${link}</p>
+            </a>
+          </li>
+        `
+      )
+      .join("")}
+      </ul>
+    </div>
+  `;
 
-    const handleStatus = (event) => {
-      if (event.status == "connected") {
-        websocket.off("status", handleStatus);
-        resolve();
-      }
-    };
-
-    websocket.on("status", handleStatus);
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const path = document.querySelector('input').value;
+    location.assign(path);
   });
-}
+} else {
+  app.innerHTML = `
+  <div class="spinner">
+    <span class="loader"></span>
+  </div>
+    `;
 
-provider.on("synced", async () => {
-  // check if document is available locally
-  const isEmpty = doc.getXmlFragment("default").length == 0;
+  const doc = new Y.Doc();
+  const provider = new WebsocketProvider("ws://localhost:1234", name, doc);
+  const _persistence = new IndexeddbPersistence(name, doc);
 
-  if (isEmpty) {
-    await notifyConnection().catch(() => {
-      if (!confirm("Can't connect to server. Edit offline?")) {
-        location.assign("/");
-      }
+  async function notifyConnection() {
+    return new Promise((resolve) => {
+      setTimeout(resolve, INIT_CONN_TIMEOUT);
+
+      const handleStatus = (event) => {
+        if (event.status == "connected") {
+          provider.off("status", handleStatus);
+          resolve();
+        }
+      };
+
+      provider.on("status", handleStatus);
     });
   }
 
-  mount(isEmpty);
-});
+  async function init() {
+    await notifyConnection();
 
-function mount(isEmpty) {
-  const app = document.querySelector('#app');
-  document.querySelector('.spinner').remove();
+    const isEmpty = doc.getXmlFragment('default').length === 0;
 
-  const element = document.createElement('div');
-  element.classList.add('editor');
-  app.appendChild(element);
+    app.innerHTML = `
+      <nav>
+        <a href="/">dotpad</a>
+      </nav>
+      <div class="editor"></div>
+    `;
 
-  new Editor({
-    element: document.querySelector(".editor"),
-    extensions: [
-      StarterKit.configure({
-        history: false, // Collaboration comes with its own history extension
-        document,
-      }),
-      Collaboration.configure({
-        document: doc,
-      }),
-    ],
-    content: "",
-    autofocus: isEmpty,
-  });
+    new Editor({
+      element: document.querySelector(".editor"),
+      extensions: [
+        StarterKit.configure({
+          history: false, // Collaboration comes with its own history extension
+          document,
+        }),
+        Collaboration.configure({
+          document: doc,
+        }),
+      ],
+      content: "",
+      autofocus: isEmpty,
+    });
+  }
+
+  init();
 }
